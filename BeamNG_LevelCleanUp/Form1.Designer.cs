74f74dcac63c03b8074b2484c2f52281b9b8a44a
Alexander Kleinwaechter <alexander.kleinwaechter@50hertz.com>
2022-11-16 22:52:38 +0100

    add version number

--- BeamNG_LevelCleanUp/Form1.Designer.cs
+++ BeamNG_LevelCleanUp/Form1.Designer.cs
@@ -41,8 +41,6 @@
             this.chkDryRun = new System.Windows.Forms.CheckBox();
             this.btn_AnalyzeLevel = new System.Windows.Forms.Button();
             this.dataGridViewDeleteList = new System.Windows.Forms.DataGridView();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.richTextBoxErrors = new System.Windows.Forms.RichTextBox();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.btnZipDeployment2 = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
@@ -53,6 +51,8 @@
             this.tb_rename_new_name_path = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.tb_rename_current_name = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBoxErrors = new System.Windows.Forms.RichTextBox();
             this.tbProgress = new System.Windows.Forms.TextBox();
             this.btnLoadLevelZipFile = new System.Windows.Forms.Button();
             this.tbLevelZipFile = new System.Windows.Forms.TextBox();
@@ -72,8 +72,8 @@
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDeleteList)).BeginInit();
-            this.tabPage2.SuspendLayout();
             this.tabPage3.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -236,26 +236,6 @@
             this.dataGridViewDeleteList.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewDeleteList_CellValueChanged);
             this.dataGridViewDeleteList.CurrentCellDirtyStateChanged += new System.EventHandler(this.dataGridViewDeleteList_CurrentCellDirtyStateChanged);
             // 
-            // tabPage2
-            // 
-            this.tabPage2.Controls.Add(this.richTextBoxErrors);
-            this.tabPage2.Location = new System.Drawing.Point(4, 29);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1063, 736);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Errors";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // richTextBoxErrors
-            // 
-            this.richTextBoxErrors.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBoxErrors.Location = new System.Drawing.Point(3, 3);
-            this.richTextBoxErrors.Name = "richTextBoxErrors";
-            this.richTextBoxErrors.Size = new System.Drawing.Size(1057, 730);
-            this.richTextBoxErrors.TabIndex = 0;
-            this.richTextBoxErrors.Text = "";
-            // 
             // tabPage3
             // 
             this.tabPage3.Controls.Add(this.btnZipDeployment2);
@@ -354,6 +334,26 @@
             this.tb_rename_current_name.Size = new System.Drawing.Size(346, 27);
             this.tb_rename_current_name.TabIndex = 0;
             // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.richTextBoxErrors);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1063, 736);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Errors";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBoxErrors
+            // 
+            this.richTextBoxErrors.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBoxErrors.Location = new System.Drawing.Point(3, 3);
+            this.richTextBoxErrors.Name = "richTextBoxErrors";
+            this.richTextBoxErrors.Size = new System.Drawing.Size(1057, 730);
+            this.richTextBoxErrors.TabIndex = 0;
+            this.richTextBoxErrors.Text = "";
+            // 
             // tbProgress
             // 
             this.tbProgress.Location = new System.Drawing.Point(10, 109);
@@ -439,7 +439,7 @@
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "BeamNG Tools for Mapbuilders";
+            this.Text = "BeamNG Tools for Mapbuilders - version 0.81";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.splitContainer1.Panel1.ResumeLayout(false);
@@ -448,9 +448,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDeleteList)).EndInit();
-            this.tabPage2.ResumeLayout(false);
             this.tabPage3.ResumeLayout(false);
             this.tabPage3.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
