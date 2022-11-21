c0c1194bdd5605c1f3db5db7204b76ee3ed73d91
Alexander Kleinwaechter <alexander.kleinwaechter@50hertz.com>
2022-11-21 15:47:38 +0100

    Neue Version

--- BeamNG_LevelCleanUp/Form1.Designer.cs
+++ BeamNG_LevelCleanUp/Form1.Designer.cs
@@ -482,7 +482,7 @@
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "BeamNG Tools for Mapbuilders - version 0.84";
+            this.Text = "BeamNG Tools for Mapbuilders - version 0.85";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.splitContainerShrink.Panel1.ResumeLayout(false);
